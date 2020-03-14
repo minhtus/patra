71ee8c081c0e93d113f2e86c900f7f259c98ed22
NamNS <apocalypsetank234@gmail.com>
2020-03-15 01:36:10 +0700

    NamNS: - Implement register user - Temporary let anonymous use API for easier developing

--- src/main/java/com/prc391/patra/users/UserService.java
+++ src/main/java/com/prc391/patra/users/UserService.java
@@ -1,18 +1,61 @@
 package com.prc391.patra.users;
 
+import com.prc391.patra.users.role.Role;
+import com.prc391.patra.users.role.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 class UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
     }
 
-    User registerUser(User user) {
+    public User registerUser(User newUserInfo) {
+        if (ObjectUtils.isEmpty(newUserInfo)) {
+            //throw exception here (if necessary)
+            return null;
+        }
+        User userInDB = userRepository.getUserByUsername(newUserInfo.getUsername());
+        if (!ObjectUtils.isEmpty(userInDB)) {
+            //TODO: throw new User Already Existed Exception
+            return null;
+        }
+
+        User user = new User();
+        user.setUsername(newUserInfo.getUsername());
+        user.setPassHash(passwordEncoder.encode(newUserInfo.getPassHash()));
+        user.setName(newUserInfo.getName());
+        //TODO: implement email verification, if possible
+        user.setEmail(newUserInfo.getEmail());
+
+        user.setEnabled(newUserInfo.isEnabled());
+        List<Long> userRoles = new ArrayList<>();
+        for (Long roleId : newUserInfo.getRoles()) {
+            Optional<Role> currentRole = roleRepository.findById(roleId);
+            if (currentRole.isPresent()) {
+                userRoles.add(roleId);
+            } else {
+                //TODO: role is not present
+            }
+        }
+        user.setRoles(userRoles);
+
+        //Do you... really waaaaant to return the hashed password?
+        //The password also contains the encoding algorithm, good luck
         return userRepository.save(user);
     }
 
