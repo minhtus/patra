4c0bd5d79012360c6d26dd6ba94b37dea6399a25
minhtus <mitu9811@gmail.com>
2020-03-15 10:18:44 +0700

    Lists api; Heroku deploy test

--- src/main/java/com/prc391/patra/lists/List.java
+++ src/main/java/com/prc391/patra/lists/List.java
@@ -1,11 +1,17 @@
 package com.prc391.patra.lists;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-@Getter
-@Setter
+@Data
 public class List {
+    @Id
+    @JsonIgnore
+    private String listId;
+    private String listName;
+    private String listDescription;
+    private String creatorUsername;
 }
