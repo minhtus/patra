9fb5a5eca209f9bf3e31281933cbe22766096fb1
NamNS <apocalypsetank234@gmail.com>
2020-03-15 21:51:20 +0700

    NamNS: implement Role REST APIs - Add EntityExistedException - Add new constructors for InvalidInputException, EntityNotFoundException

--- src/main/java/com/prc391/patra/exceptions/EntityNotFoundException.java
+++ src/main/java/com/prc391/patra/exceptions/EntityNotFoundException.java
@@ -1,4 +1,11 @@
 package com.prc391.patra.exceptions;
 
 public class EntityNotFoundException extends Exception{
+
+    public EntityNotFoundException() {
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
 }
