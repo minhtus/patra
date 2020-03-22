dc18661ed854f9e81c024810d1b580ce6ac40797
NamNS <apocalypsetank234@gmail.com>
2020-03-23 01:18:38 +0700

    NamNS: implement member API

---
+++ src/main/java/com/prc391/patra/members/requests/CreateMemberRequest.java
@@ -0,0 +1,10 @@
+package com.prc391.patra.members.requests;
+
+import lombok.Data;
+
+@Data
+public class CreateMemberRequest {
+    private String orgId;
+    private String username;
+    private Long[] permissions;
+}
