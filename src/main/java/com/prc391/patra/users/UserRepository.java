1176e577ec5854a8cc44f47650363f2676c269f2
minhtus <mitu9811@gmail.com>
2020-03-23 22:42:07 +0700

    Change field name; add todo

--- src/main/java/com/prc391/patra/users/UserRepository.java
+++ src/main/java/com/prc391/patra/users/UserRepository.java
@@ -3,5 +3,5 @@ package com.prc391.patra.users;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
-//    User getUserByUsername(String username);
+    User getUserByEmail(String email);
 }
