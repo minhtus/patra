4a99920e86a0dbe8a2c4401c006c722ceed5d9b9
minhtus <mitu9811@gmail.com>
2020-03-23 22:42:07 +0700

    Change field name; add todo

--- src/main/java/com/prc391/patra/users/User.java
+++ src/main/java/com/prc391/patra/users/User.java
@@ -5,17 +5,17 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
 @Document
 @Data
 public class User {
     @Id
-    private String id;
+    private String username;
+    @JsonIgnore
     private String passHash;
     private String email;
     private String name;
 
+    @JsonIgnore
     private boolean enabled;
 
     //TODO: redesign the Permission.
--- src/main/java/com/prc391/patra/users/UserController.java
+++ src/main/java/com/prc391/patra/users/UserController.java
@@ -2,40 +2,37 @@ package com.prc391.patra.users;
 
 import com.prc391.patra.exceptions.EntityExistedException;
 import com.prc391.patra.exceptions.EntityNotFoundException;
-import com.prc391.patra.users.request.UserResponse;
+import com.prc391.patra.users.requests.CreateUserRequest;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v0/users")
 public class UserController {
 
     private final UserService userService;
-    private final AuthenticationService authenticationService;
     private final ModelMapper mapper;
 
     @Autowired
-    public UserController(UserService userService, AuthenticationService authenticationService, ModelMapper mapper) {
+    public UserController(UserService userService, ModelMapper mapper) {
         this.userService = userService;
-        this.authenticationService = authenticationService;
         this.mapper = mapper;
     }
 
-
     @GetMapping("/{username}")
-    public ResponseEntity<UserResponse> getUserByUsername(@PathVariable("username") String username) throws EntityNotFoundException {
-        return ResponseEntity.ok(mapper.map(userService.getUser(username), UserResponse.class));
+    public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username) throws EntityNotFoundException {
+        //TODO get by username or email
+        return ResponseEntity.ok(userService.getUser(username));
     }
 
     @PostMapping
-    public ResponseEntity<UserResponse> registerUser(@RequestBody User user) throws EntityExistedException {
-        return ResponseEntity.ok(mapper.map(userService.registerUser(user), UserResponse.class));
+    public ResponseEntity<User> registerUser(@RequestBody CreateUserRequest request) throws EntityExistedException {
+        return ResponseEntity.ok(userService.registerUser(mapper.map(request, User.class)));
     }
+
+    //TODO get all org of user (members) maybe done?
+    //TODO get all lists of user
+    //TODO get all task of user
 }
