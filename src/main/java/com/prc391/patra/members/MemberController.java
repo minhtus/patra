4ddf893dd5984921a476327b555d1c925a90c0d5
NamNS <apocalypsetank234@gmail.com>
2020-03-24 10:45:17 +0700

    NamNS: fix API path naming

--- src/main/java/com/prc391/patra/members/MemberController.java
+++ src/main/java/com/prc391/patra/members/MemberController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v0/member")
+@RequestMapping("/v0/members")
 public class MemberController {
 
     private final MemberService memberService;
