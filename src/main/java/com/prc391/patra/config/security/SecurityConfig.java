fb97ab1c64209060a988a0c50c1cb066a8469fa7
NamNS <apocalypsetank234@gmail.com>
2020-03-26 08:23:57 +0700

    NamNS:  - add allow cors for pre-flight requests

--- src/main/java/com/prc391/patra/config/security/SecurityConfig.java
+++ src/main/java/com/prc391/patra/config/security/SecurityConfig.java
@@ -16,6 +16,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -49,11 +54,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(databaseAuthProvider);
     }
 
+    //allow preflight request
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList(
+                "GET","POST","PUT","DELETE","PATCH"
+        ));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList(
+                "Authorization", "Cache-Control", "Content-type"
+        ));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //temporary disable this to let anonymous use POST methods
         //re-enable it (delete this line) before going "production"
         http.csrf().disable();
+
+        http.cors();
+
         http.authorizeRequests()
 
                 .antMatchers("/login").permitAll()
