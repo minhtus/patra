637c88ba579b9022be8b9954daef1ab2a5370bb7
NamNS <apocalypsetank234@gmail.com>
2020-03-29 02:10:30 +0700

    [fix]NamNS: update id in Organization to orgId

--- src/main/java/com/prc391/patra/orgs/Organization.java
+++ src/main/java/com/prc391/patra/orgs/Organization.java
@@ -2,24 +2,20 @@ package com.prc391.patra.orgs;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Document
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Organization {
     @Id
-    private String id;
+    private String orgId;
     private String name;
     private String imageUrl;
 
     public void mergeForUpdate(Organization other) {
-        this.id = other.id != null? other.id : this.id;
+        this.orgId = other.orgId != null? other.orgId : this.orgId;
         this.name = other.name != null? other.name : this.name;
         this.imageUrl = other.imageUrl != null? other.imageUrl : this.imageUrl;
     }
