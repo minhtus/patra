b78b088eb37270a81f26da40cdf49feb4038bb89
NamNS <apocalypsetank234@gmail.com>
2020-03-31 00:57:16 +0700

    NamNS: implement password, will push to master when redis logic is complete

--- src/main/java/com/prc391/patra/config/RedisConfig.java
+++ src/main/java/com/prc391/patra/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -19,9 +20,15 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        RedisPassword redisPasswordObj = RedisPassword.of(redisPassword);
+        configuration.setPassword(redisPasswordObj);
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(configuration);
 //        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
         return jedisConnectionFactory;
