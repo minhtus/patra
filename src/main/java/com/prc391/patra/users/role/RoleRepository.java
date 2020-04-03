10f6e8dfdb973eda5e764dcc401cfd0bb168b4e8
NamNS <apocalypsetank234@gmail.com>
2020-04-04 01:42:49 +0700

    NamNS: remove role

--- src/main/java/com/prc391/patra/users/role/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prc391.patra.users.role;
-
-import com.prc391.patra.users.permission.Permission;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role, Long> {
-    Role getRoleByName(String name);
-}
