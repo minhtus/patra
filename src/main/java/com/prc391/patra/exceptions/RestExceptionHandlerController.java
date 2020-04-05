c7b7fd76aba252034d1a9816c95792e358f6e014
NamNS <apocalypsetank234@gmail.com>
2020-04-05 17:42:51 +0700

    NamNS: add handle for Redis connection exception

--- src/main/java/com/prc391/patra/exceptions/RestExceptionHandlerController.java
+++ src/main/java/com/prc391/patra/exceptions/RestExceptionHandlerController.java
@@ -1,6 +1,7 @@
 package com.prc391.patra.exceptions;
 
 import com.prc391.patra.utils.BaseResponse;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,5 +26,11 @@ public class RestExceptionHandlerController {
     public ResponseEntity badRequestHandler(Exception ex) {
         return ResponseEntity.badRequest()
                 .body(new BaseResponse(false, "Invalid params or payload " + ex.getMessage()));
+    }
+
+    @ExceptionHandler(RedisConnectionFailureException.class)
+    public ResponseEntity redisConnectionFailureHandler(Exception ex) {
+        return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT)
+                .body(new BaseResponse(false, "Timeout " + ex.getMessage()));
     }
 }
